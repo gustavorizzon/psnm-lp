5bf8547c790a0d655799f1207917bf782800c58d
Gustavo Rizzon <gustavop.rizzon@gmail.com>
2025-02-22 18:25:32 -0300

    build: fix build to production and adjust some commands

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  site: "https://psnm.guzz.dev",
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      external: ["prismjs"],
+    },
   },
 });
